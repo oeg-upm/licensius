40330f30eb5320aa26f2b4fd506577df45bad044
Víctor <victorr@ac.upc.edu>
2015-04-15 22:50:10 +0200

    Incluye lectura del archivo de configuración

--- src/ldconditional/Main.java
+++ src/ldconditional/Main.java
@@ -30,6 +30,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         initLogger();
         
+        LDRConfig.Load();
+        
         initDatasets();
         
         initServer();
