c9e1ea11dc8af83af0a4ceb74919a29980edd008
vroddon <victorr@ac.upc.edu>
2015-10-27 10:15:42 +0100

    Añadido el swagger. Añadido los porimeros servicios

--- src/java/oeg/licensius/service/LicenseList.java
+++ src/java/oeg/licensius/service/LicenseList.java
@@ -2,24 +2,16 @@ package oeg.licensius.service;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URI;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import oeg.license.LicenseFinder;
-import oeg.license.LicenseFinderWeb;
-import oeg.rdflicense.RDFLicense;
-import oeg.rdflicense.RDFLicenseDataset;
 
-/** 
- * Test service to be docummented by swagger, deployed in Tomcat and running at www.licensius.com
- * Buen puntero: http://www.jqueryscript.net/
+/**
+ * Returns a list of licenses
  * @author vrodriguez
  */
-public class Test extends HttpServlet {
+public class LicenseList extends HttpServlet {
     
     private static final Logger logger = Logger.getLogger(Test.class.getName());
 
@@ -30,9 +22,8 @@ public class Test extends HttpServlet {
             resp.setStatus(200);
             resp.setContentType("text/html");
             PrintWriter w = resp.getWriter();
-            w.println("Thanks for testing licensius");
+            w.println("This returns a list of licenses");
             return;
         }
-
     
 }
