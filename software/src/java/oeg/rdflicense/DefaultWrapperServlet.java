81f2b4df68717dac7a3eb627fc0763829f406bed
vroddon <victorr@ac.upc.edu>
2015-11-19 14:51:18 +0100

    asdf

--- software/src/java/oeg/rdflicense/DefaultWrapperServlet.java
+++ software/src/java/oeg/rdflicense/DefaultWrapperServlet.java
@@ -7,13 +7,15 @@ import org.apache.log4j.Logger;
 
 
 /**
- * @todo Document the mission of this class
+ * This class serves files like .png, .jpg, etc. 
+ * Typicially this is done with the default servlet at javax.servlet-api. 
+ * But this is not accepted by appspot
  */
 public class DefaultWrapperServlet extends HttpServlet
 {   private static final Logger logger = Logger.getLogger(DefaultWrapperServlet.class.getName());
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-    	RequestDispatcher rd = getServletContext().getNamedDispatcher("default");
+    	RequestDispatcher rd = getServletContext().getNamedDispatcher("defaultX");
         logger.info("GRAPER2 siendo ejecutado por alguna razón misteriosa " + req.getRequestURI());
         
         String uri = req.getRequestURI(); 
@@ -23,12 +25,16 @@ public class DefaultWrapperServlet extends HttpServlet
             return;
         }
 
-/*        if (uri.endsWith(".png")) {
+       if (uri.endsWith(".png")) {
             resp.setContentType("image/png");
+            
+            
+            
             ServletOutputStream output = resp.getOutputStream();
             String filename = uri.substring("/rdflicense/".length());
             logger.info("GRAPER3 intentando abrir " + filename);
-            InputStream input = new FileInputStream(filename);
+//            InputStream input = new FileInputStream(filename);
+            InputStream input = this.getClass().getResourceAsStream("rdflicense32.png");
             if (input==null)
                 return;
             byte[] buffer = new byte[2048];
@@ -38,8 +44,8 @@ public class DefaultWrapperServlet extends HttpServlet
             }
             return;
         }
-        if (true)
-            return;*/
+       // if (true)
+       //     return; 
         
     	HttpServletRequest wrapped = new HttpServletRequestWrapper(req) {
     		public String getServletPath() { return ""; }
