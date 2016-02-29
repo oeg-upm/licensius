53d268b8d79b689219ccf7e0d1786b11f7bdf3c4
vroddon <victorr@ac.upc.edu>
2016-02-29 11:11:01 +0100

    refactor. We start by deletein everyghing.

--- doc.bat
+++
@@ -1,5 +0,0 @@
-REM Primero hay que instalar npm para windows aquí https://www.npmjs.com/package/npm
-REM No hay que descargar nada. Pero podrías ver el código aquí here https://github.com/apidoc/apidoc
-REM Follow the instructions here:  http://apidocjs.com/
-apidoc -i . apidoc/
-ftp -i -s:doc.ftp
