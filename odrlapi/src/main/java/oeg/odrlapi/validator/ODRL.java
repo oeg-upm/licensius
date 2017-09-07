5e9d425e98fa83da6dd641283d56fcda49a1401b
Víctor Rodríguez Doncel <victorr@ac.upc.edu>
2017-09-07 14:14:02 -0400

    Verification on AssetCollection

--- odrlapi/src/main/java/oeg/odrlapi/validator/ODRL.java
+++ odrlapi/src/main/java/oeg/odrlapi/validator/ODRL.java
@@ -1,7 +1,9 @@
 package oeg.odrlapi.validator;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
@@ -26,6 +28,7 @@ public class ODRL {
     public static Resource RDUTY = ModelFactory.createDefaultModel().createResource("http://www.w3.org/ns/odrl/2/Duty");
     public static Resource RCONSTRAINT = ModelFactory.createDefaultModel().createResource("http://www.w3.org/ns/odrl/2/Constraint");
     public static Resource RLOGICALCONSTRAINT = ModelFactory.createDefaultModel().createResource("http://www.w3.org/ns/odrl/2/LogicalConstraint");
+    public static Resource RASSETCOLLECTION = ModelFactory.createDefaultModel().createResource("http://www.w3.org/ns/odrl/2/AssetCollection");
     
     public static Resource RACTION = ModelFactory.createDefaultModel().createResource("http://www.w3.org/ns/odrl/2/Action");
     public static Property PTARGET = ModelFactory.createDefaultModel().createProperty("http://www.w3.org/ns/odrl/2/target");
@@ -54,6 +57,7 @@ public class ODRL {
     public static Property PINHERITFROM = ModelFactory.createDefaultModel().createProperty("http://www.w3.org/ns/odrl/2/inheritFrom");
     public static Property PPROFILE = ModelFactory.createDefaultModel().createProperty("http://www.w3.org/ns/odrl/2/profile");
     public static Property PCONFLICT = ModelFactory.createDefaultModel().createProperty("http://www.w3.org/ns/odrl/2/conflict");
+    public static Property PSOURCE = ModelFactory.createDefaultModel().createProperty("http://www.w3.org/ns/odrl/2/source");
     
             
     public static Property PDCLICENSE = ModelFactory.createDefaultModel().createProperty("http://purl.org/dc/terms/license");
@@ -91,5 +95,120 @@ public class ODRL {
         return list;
     }
     
+    /**
+     * Returns the ODRL actions. Does NOT load the ontology in order to speed
+     * up.
+     */
+    public static Set<String> getActions() {
+        Set<String> set = new HashSet();
+        set.add("http://www.w3.org/ns/odrl/2/transfer");
+        set.add("http://www.w3.org/ns/odrl/2/use");
+        
+        set.add("http://www.w3.org/ns/odrl/2/acceptTracking");
+        set.add("http://www.w3.org/ns/odrl/2/aggregate");
+        set.add("http://www.w3.org/ns/odrl/2/annotate");
+        set.add("http://www.w3.org/ns/odrl/2/anonymize");
+        set.add("http://www.w3.org/ns/odrl/2/archive");
+        set.add("http://www.w3.org/ns/odrl/2/attribute");
+        set.add("http://creativecommons.org/ns#Attribution");
+        set.add("http://creativecommons.org/ns#CommericalUse");
+        set.add("http://www.w3.org/ns/odrl/2/compensate");
+        set.add("http://www.w3.org/ns/odrl/2/concurrentUse");
+        set.add("http://www.w3.org/ns/odrl/2/delete");
+        set.add("http://www.w3.org/ns/odrl/2/derive");
+        set.add("http://creativecommons.org/ns#DerivativeWorks");
+        set.add("http://www.w3.org/ns/odrl/2/digitize");
+        set.add("http://www.w3.org/ns/odrl/2/display");
+        set.add("http://www.w3.org/ns/odrl/2/distribute");
+        set.add("http://creativecommons.org/ns#Distribution");
+        set.add("http://www.w3.org/ns/odrl/2/ensureExclusivity");
+        set.add("http://www.w3.org/ns/odrl/2/execute");
+        set.add("http://www.w3.org/ns/odrl/2/extract");
+        set.add("http://www.w3.org/ns/odrl/2/give");
+        set.add("http://www.w3.org/ns/odrl/2/grantUse");
+        set.add("http://www.w3.org/ns/odrl/2/include");
+        set.add("http://www.w3.org/ns/odrl/2/index");
+        set.add("http://www.w3.org/ns/odrl/2/inform");
+        set.add("http://www.w3.org/ns/odrl/2/install");
+        set.add("http://www.w3.org/ns/odrl/2/modify");
+        set.add("http://www.w3.org/ns/odrl/2/move");
+        set.add("http://www.w3.org/ns/odrl/2/nextPolicy");
+        set.add("http://creativecommons.org/ns#Notice");
+        set.add("http://www.w3.org/ns/odrl/2/obtainConsent");
+        set.add("http://www.w3.org/ns/odrl/2/play");
+        set.add("http://www.w3.org/ns/odrl/2/present");
+        set.add("http://www.w3.org/ns/odrl/2/print");
+        set.add("http://www.w3.org/ns/odrl/2/read");
+        set.add("http://www.w3.org/ns/odrl/2/reproduce");
+        set.add("http://creativecommons.org/ns#Reproduction");
+        set.add("http://www.w3.org/ns/odrl/2/reviewPolicy");
+        set.add("http://www.w3.org/ns/odrl/2/sell");
+        set.add("http://creativecommons.org/ns#ShareAlike");
+        set.add("http://creativecommons.org/ns#Sharing");
+        set.add("http://creativecommons.org/ns#SourceCode");
+        set.add("http://www.w3.org/ns/odrl/2/stream");
+        set.add("http://www.w3.org/ns/odrl/2/synchronize");
+        set.add("http://www.w3.org/ns/odrl/2/textToSpeech");
+        set.add("http://www.w3.org/ns/odrl/2/transform");
+        set.add("http://www.w3.org/ns/odrl/2/translate");
+        set.add("http://www.w3.org/ns/odrl/2/uninstall");
+        set.add("http://www.w3.org/ns/odrl/2/watermark");
+        return set;
+    }
+
+    public static Set<String> getOperators() {
+        Set<String> set = new HashSet();
+        set.add("http://www.w3.org/ns/odrl/2/eq");
+        set.add("http://www.w3.org/ns/odrl/2/gt");
+        set.add("http://www.w3.org/ns/odrl/2/gteq");
+        set.add("http://www.w3.org/ns/odrl/2/lt");
+        set.add("http://www.w3.org/ns/odrl/2/lteq");
+        set.add("http://www.w3.org/ns/odrl/2/neq");
+        set.add("http://www.w3.org/ns/odrl/2/isA");
+        set.add("http://www.w3.org/ns/odrl/2/hasPart");
+        set.add("http://www.w3.org/ns/odrl/2/isPartOf");
+        set.add("http://www.w3.org/ns/odrl/2/isAllOf");
+        set.add("http://www.w3.org/ns/odrl/2/isAnyOf");
+        set.add("http://www.w3.org/ns/odrl/2/isNoneOf");
+        return set;
+    }
+
+    public static Set<String> getLeftOperands() {
+        Set<String> set = new HashSet();
+        set.add("http://www.w3.org/ns/odrl/2/absolutePosition");
+        set.add("http://www.w3.org/ns/odrl/2/absoluteSpatialPosition");
+        set.add("http://www.w3.org/ns/odrl/2/absoluteTemporalPosition");
+        set.add("http://www.w3.org/ns/odrl/2/absoluteSize");
+        set.add("http://www.w3.org/ns/odrl/2/count");
+        set.add("http://www.w3.org/ns/odrl/2/dateTime");
+        set.add("http://www.w3.org/ns/odrl/2/delayPeriod");
+        set.add("http://www.w3.org/ns/odrl/2/deliveryChannel");
+        set.add("http://www.w3.org/ns/odrl/2/elapsedTime");
+        set.add("http://www.w3.org/ns/odrl/2/event");
+        set.add("http://www.w3.org/ns/odrl/2/fileFormat");
+        set.add("http://www.w3.org/ns/odrl/2/industry");
+        set.add("http://www.w3.org/ns/odrl/2/language");
+        set.add("http://www.w3.org/ns/odrl/2/media");
+        set.add("http://www.w3.org/ns/odrl/2/meteredTime");
+        set.add("http://www.w3.org/ns/odrl/2/payAmount");
+        set.add("http://www.w3.org/ns/odrl/2/percentage");
+        set.add("http://www.w3.org/ns/odrl/2/product");
+        set.add("http://www.w3.org/ns/odrl/2/purpose");
+        set.add("http://www.w3.org/ns/odrl/2/recipient");
+        set.add("http://www.w3.org/ns/odrl/2/relativePosition");
+        set.add("http://www.w3.org/ns/odrl/2/relativeSpatialPosition");
+        set.add("http://www.w3.org/ns/odrl/2/relativeTemporalPosition");
+        set.add("http://www.w3.org/ns/odrl/2/relativeSize");
+        set.add("http://www.w3.org/ns/odrl/2/resolution");
+        set.add("http://www.w3.org/ns/odrl/2/spatial");
+        set.add("http://www.w3.org/ns/odrl/2/spatialCoordinates");
+        set.add("http://www.w3.org/ns/odrl/2/systemDevice");
+        set.add("http://www.w3.org/ns/odrl/2/timeInterval");
+        set.add("http://www.w3.org/ns/odrl/2/unitOfCount");
+        set.add("http://www.w3.org/ns/odrl/2/version");
+        set.add("http://www.w3.org/ns/odrl/2/virtualLocation");
+
+        return set;
+    }
     
 }
--- odrlapi/src/main/java/oeg/odrlapi/validator/Validation07.java
+++ odrlapi/src/main/java/oeg/odrlapi/validator/Validation07.java
@@ -2,7 +2,6 @@ package oeg.odrlapi.validator;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import oeg.odrlapi.rdf.RDFUtils;
 import oeg.odrlapi.rest.server.resources.ValidatorResponse;
@@ -20,125 +19,13 @@ import org.apache.jena.vocabulary.RDF;
  */
 public class Validation07 implements Validation {
 
-    /**
-     * Returns the ODRL actions. Does NOT load the ontology in order to speed
-     * up.
-     */
-    public static Set<String> getActions() {
-        Set<String> set = new HashSet();
-        set.add("http://www.w3.org/ns/odrl/2/acceptTracking");
-        set.add("http://www.w3.org/ns/odrl/2/aggregate");
-        set.add("http://www.w3.org/ns/odrl/2/annotate");
-        set.add("http://www.w3.org/ns/odrl/2/anonymize");
-        set.add("http://www.w3.org/ns/odrl/2/archive");
-        set.add("http://www.w3.org/ns/odrl/2/attribute");
-        set.add("http://creativecommons.org/ns#Attribution");
-        set.add("http://creativecommons.org/ns#CommericalUse");
-        set.add("http://www.w3.org/ns/odrl/2/compensate");
-        set.add("http://www.w3.org/ns/odrl/2/concurrentUse");
-        set.add("http://www.w3.org/ns/odrl/2/delete");
-        set.add("http://www.w3.org/ns/odrl/2/derive");
-        set.add("http://creativecommons.org/ns#DerivativeWorks");
-        set.add("http://www.w3.org/ns/odrl/2/digitize");
-        set.add("http://www.w3.org/ns/odrl/2/display");
-        set.add("http://www.w3.org/ns/odrl/2/distribute");
-        set.add("http://creativecommons.org/ns#Distribution");
-        set.add("http://www.w3.org/ns/odrl/2/ensureExclusivity");
-        set.add("http://www.w3.org/ns/odrl/2/execute");
-        set.add("http://www.w3.org/ns/odrl/2/extract");
-        set.add("http://www.w3.org/ns/odrl/2/give");
-        set.add("http://www.w3.org/ns/odrl/2/grantUse");
-        set.add("http://www.w3.org/ns/odrl/2/include");
-        set.add("http://www.w3.org/ns/odrl/2/index");
-        set.add("http://www.w3.org/ns/odrl/2/inform");
-        set.add("http://www.w3.org/ns/odrl/2/install");
-        set.add("http://www.w3.org/ns/odrl/2/modify");
-        set.add("http://www.w3.org/ns/odrl/2/move");
-        set.add("http://www.w3.org/ns/odrl/2/nextPolicy");
-        set.add("http://creativecommons.org/ns#Notice");
-        set.add("http://www.w3.org/ns/odrl/2/obtainConsent");
-        set.add("http://www.w3.org/ns/odrl/2/play");
-        set.add("http://www.w3.org/ns/odrl/2/present");
-        set.add("http://www.w3.org/ns/odrl/2/print");
-        set.add("http://www.w3.org/ns/odrl/2/read");
-        set.add("http://www.w3.org/ns/odrl/2/reproduce");
-        set.add("http://creativecommons.org/ns#Reproduction");
-        set.add("http://www.w3.org/ns/odrl/2/reviewPolicy");
-        set.add("http://www.w3.org/ns/odrl/2/sell");
-        set.add("http://creativecommons.org/ns#ShareAlike");
-        set.add("http://creativecommons.org/ns#Sharing");
-        set.add("http://creativecommons.org/ns#SourceCode");
-        set.add("http://www.w3.org/ns/odrl/2/stream");
-        set.add("http://www.w3.org/ns/odrl/2/synchronize");
-        set.add("http://www.w3.org/ns/odrl/2/textToSpeech");
-        set.add("http://www.w3.org/ns/odrl/2/transform");
-        set.add("http://www.w3.org/ns/odrl/2/translate");
-        set.add("http://www.w3.org/ns/odrl/2/uninstall");
-        set.add("http://www.w3.org/ns/odrl/2/watermark");
-        return set;
-    }
-
-    public static Set<String> getOperators() {
-        Set<String> set = new HashSet();
-        set.add("http://www.w3.org/ns/odrl/2/eq");
-        set.add("http://www.w3.org/ns/odrl/2/gt");
-        set.add("http://www.w3.org/ns/odrl/2/gteq");
-        set.add("http://www.w3.org/ns/odrl/2/lt");
-        set.add("http://www.w3.org/ns/odrl/2/lteq");
-        set.add("http://www.w3.org/ns/odrl/2/neq");
-        set.add("http://www.w3.org/ns/odrl/2/isA");
-        set.add("http://www.w3.org/ns/odrl/2/hasPart");
-        set.add("http://www.w3.org/ns/odrl/2/isPartOf");
-        set.add("http://www.w3.org/ns/odrl/2/isAllOf");
-        set.add("http://www.w3.org/ns/odrl/2/isAnyOf");
-        set.add("http://www.w3.org/ns/odrl/2/isNoneOf");
-        return set;
-    }
-
-    public static Set<String> getLeftOperands() {
-        Set<String> set = new HashSet();
-        set.add("http://www.w3.org/ns/odrl/2/absolutePosition");
-        set.add("http://www.w3.org/ns/odrl/2/absoluteSpatialPosition");
-        set.add("http://www.w3.org/ns/odrl/2/absoluteTemporalPosition");
-        set.add("http://www.w3.org/ns/odrl/2/absoluteSize");
-        set.add("http://www.w3.org/ns/odrl/2/count");
-        set.add("http://www.w3.org/ns/odrl/2/dateTime");
-        set.add("http://www.w3.org/ns/odrl/2/delayPeriod");
-        set.add("http://www.w3.org/ns/odrl/2/deliveryChannel");
-        set.add("http://www.w3.org/ns/odrl/2/elapsedTime");
-        set.add("http://www.w3.org/ns/odrl/2/event");
-        set.add("http://www.w3.org/ns/odrl/2/fileFormat");
-        set.add("http://www.w3.org/ns/odrl/2/industry");
-        set.add("http://www.w3.org/ns/odrl/2/language");
-        set.add("http://www.w3.org/ns/odrl/2/media");
-        set.add("http://www.w3.org/ns/odrl/2/meteredTime");
-        set.add("http://www.w3.org/ns/odrl/2/payAmount");
-        set.add("http://www.w3.org/ns/odrl/2/percentage");
-        set.add("http://www.w3.org/ns/odrl/2/product");
-        set.add("http://www.w3.org/ns/odrl/2/purpose");
-        set.add("http://www.w3.org/ns/odrl/2/recipient");
-        set.add("http://www.w3.org/ns/odrl/2/relativePosition");
-        set.add("http://www.w3.org/ns/odrl/2/relativeSpatialPosition");
-        set.add("http://www.w3.org/ns/odrl/2/relativeTemporalPosition");
-        set.add("http://www.w3.org/ns/odrl/2/relativeSize");
-        set.add("http://www.w3.org/ns/odrl/2/resolution");
-        set.add("http://www.w3.org/ns/odrl/2/spatial");
-        set.add("http://www.w3.org/ns/odrl/2/spatialCoordinates");
-        set.add("http://www.w3.org/ns/odrl/2/systemDevice");
-        set.add("http://www.w3.org/ns/odrl/2/timeInterval");
-        set.add("http://www.w3.org/ns/odrl/2/unitOfCount");
-        set.add("http://www.w3.org/ns/odrl/2/version");
-        set.add("http://www.w3.org/ns/odrl/2/virtualLocation");
-
-        return set;
-    }
 
     @Override
     public ValidatorResponse validate(String turtle) {
         Model model = ODRLValidator.getModel(turtle);
-        Set acciones = getActions();
-        Set lefts = getLeftOperands();
-        Set operators = getOperators();
+        Set acciones = ODRL.getActions();
+        Set lefts = ODRL.getLeftOperands();
+        Set operators = ODRL.getOperators();
 
         //Para cada politica
         Set<String> politicas = Preprocessing.getPoliticas(model);
