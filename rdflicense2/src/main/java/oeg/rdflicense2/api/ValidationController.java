269c79e4e268bd2bb81db1d3ef110c58938279c3
Víctor Rodríguez Doncel <victorr@ac.upc.edu>
2021-07-31 14:22:15 +0300

    asdf

--- rdflicense2/src/main/java/oeg/rdflicense2/api/ValidationController.java
+++ rdflicense2/src/main/java/oeg/rdflicense2/api/ValidationController.java
@@ -1,11 +1,21 @@
 package oeg.rdflicense2.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.io.File;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.w3c.dom.Document;
 
 /**
@@ -13,21 +23,27 @@ import org.w3c.dom.Document;
  * @author vroddon
  */
 @Controller
+@Api(tags = "Validation", value = "Validation")
+@ApiOperation(value = "Validates a catalogue entry")
 public class ValidationController {
 
-    @RequestMapping("/validate")
-    public String page(Model model) {
-        model.addAttribute("attribute", "value");
-        return "view.name";
+    @CrossOrigin
+    @ApiOperation(value = "Validates an input schema")
+    @RequestMapping(value = "/validate",method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity validate(@RequestBody String xml) {
+        boolean b = validateXML(xml);
+        return new ResponseEntity<>( b+"" ,HttpStatus.OK);
+        
+        
     }
 
-    public static boolean validate(String xml) {
+    public static boolean validateXML(String xml) {
         try {
             System.out.println("Vamos a validar!");
-            File fXmlFile = new File("/path/to/my.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(fXmlFile);
+            Document doc = dBuilder.parse(xml);
             return true;
         } catch (Exception e) {
             return false;
