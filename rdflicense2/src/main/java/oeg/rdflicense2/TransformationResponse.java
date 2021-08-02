d5995eb2fa275e0845caf1d8404be890f69523fc
Víctor Rodríguez Doncel <victorr@ac.upc.edu>
2021-08-02 12:06:20 +0300

    new, untested version

--- rdflicense2/src/main/java/oeg/rdflicense2/TransformationResponse.java
+++ rdflicense2/src/main/java/oeg/rdflicense2/TransformationResponse.java
@@ -1,7 +1,9 @@
 package oeg.rdflicense2;
 
 /**
- *
+ * Result of a transformation, including:
+ * @param valid Whether there was an error or not
+ * @param transformation Result of the transformation, or an error message.
  * @author vroddon
  */
 public class TransformationResponse {
--- rdflicense2/src/main/java/oeg/rdflicense2/api/TransformationController.java
+++ rdflicense2/src/main/java/oeg/rdflicense2/api/TransformationController.java
@@ -3,9 +3,13 @@ package oeg.rdflicense2.api;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import oeg.rdflicense2.TransformationResponse;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
@@ -36,8 +41,37 @@ public class TransformationController {
         TransformationResponse tr = transformXML(xml);
         return new ResponseEntity<>(tr, HttpStatus.OK);
     }
+    
+    
+    private static String getAttributeValue(Node n, String atext)
+    {
+            NamedNodeMap attributes = n.getAttributes();
+            for(int k=0;k<attributes.getLength();k++)
+            {
+                String atributo = attributes.item(k).getNodeName();
+                if (atributo.equals(atext))
+                {
+                    return attributes.item(k).getNodeValue();
+                }
+            }        
+            return "";
+    }
 
     public static TransformationResponse transformXML(String xml) {
+        
+        Map<String, String> pairs = new HashMap();
+        pairs.put("ms:conditionOfUse", "http://w3id.org/meta-share/meta-share/conditionOfUse");
+        pairs.put("ms:licenceCategory", "http://w3id.org/meta-share/meta-share/licenceCategory");
+        pairs.put("ms:licenceTermsURL", "http://w3id.org/meta-share/meta-share/licenceTermsURL");
+        pairs.put("ms:licenceTermsName", "http://w3id.org/meta-share/meta-share/licenceTermsName");
+        pairs.put("ms:licenceTermsShortName", "http://w3id.org/meta-share/meta-share/licenceTermsShortName");
+        pairs.put("ms:licenceTermsAlternativeName", "http://w3id.org/meta-share/meta-share/licenceTermsAlternativeName");
+   
+        
+        
+//???   ms:LicenceIdentifier --> get the attribute LicenceIdentifierScheme and check it is http://w3id.org/meta-share/meta-share/SPDX
+//
+                
         try {
             String rdf = "";
             System.out.println("Vamos a transformar!");
@@ -59,14 +93,34 @@ public class TransformationController {
                 for(int j=0;j<nl1.getLength();j++)
                 {
                     Node n1 = nl1.item(j);
-                    System.out.println(n1.toString());
+/*                    System.out.println(n1.toString());
                     System.out.println(n1.getNodeName());
-                    System.out.println(n1.getTextContent());
-                    if (n1.getNodeName().equals("ms:licenceTermsName"))
+                    System.out.println(n1.getTextContent());*/
+                    String nodename = n1.getNodeName();
+                    String nodetext = n1.getTextContent();
+                    for(String clave : pairs.keySet())
                     {
-                        rdf += "_:license <http://purl.org/dc/terms/title> \""+ n1.getTextContent() +  "\" .\n";
+                        if (clave.equals(nodename))
+                        {
+                            String lang = getAttributeValue(n1, "xml:lang");
+                            if (!lang.isEmpty())
+                                lang="@"+lang;
+                            
+//                           Resource r = ModelFactory.createDefaultModel().createResource("_:licence");
+//                           Resource r = ModelFactorycreateDefaultModel().createResource("_:licence");
+                            if (nodetext.startsWith("http"))
+                                rdf += "_:license <" + pairs.get(clave)+"> <"+ nodetext +  "> .\n";
+                            else
+                                rdf += "_:license <" + pairs.get(clave)+"> \""+ nodetext +  "\""+lang+" .\n";
+                        }
                     }
-                    
+                    if (nodename.equals("ms:LicenceIdentifier"))
+                    {
+                        String propiedad = "http://w3id.org/meta-share/meta-share/LicenceIdentifier";
+                        String identifier = getAttributeValue(n1, "ms:LicenceIdentifierScheme");
+                        rdf += "_:license <" + propiedad +"> <"+ identifier +  "> .\n";
+                    }
+                            
                 }
             }
             TransformationResponse tr = new TransformationResponse(true, rdf);
